20d03023a41911dd8302b1aa67455032e86ff372
Alex Lubrano <lubranoa@oregonstate.edu>
2022-12-03 17:04:11 -0600

    Create files and add boilerplate comments

---
+++ README.md
@@ -0,0 +1,7 @@
+# CS 372 Portfolio Project: Client/Server Chat
+
+## Description
+Text goes here.
+
+## Resources
+Text goes here.
---
+++ chat_server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# ----------------------------------------------------------------------------
+# Author: Alexander Lubrano
+# Course: CS 372 - 401
+# Assignment: Client/Server Chat: chat_server.py
+# Date: 12/03/2022
+#
+# Sources Used:
+#     -
+#
+# ----------------------------------------------------------------------------
