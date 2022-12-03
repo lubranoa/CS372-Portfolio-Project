b43c6e47c605f2578136bc404dedb7164df5f4c0
Alex Lubrano <lubranoa@oregonstate.edu>
2022-12-03 17:14:22 -0600

    Add pseudocode comments

--- chat_client.py
+++ chat_client.py
@@ -7,6 +7,24 @@
 # Date: 12/03/2022
 #
 # Sources Used:
-#     -
+#     - 
 #
 # ----------------------------------------------------------------------------
+
+# Create socket
+
+# Connect to localhost and port xxxx
+
+# While connected:
+
+    # Prompt user for a message to send
+
+    # If message is "/q", client quits
+
+    # Else, client sends message
+
+    # Call recv to receive any data
+
+    # Print data
+
+# Close sockets
--- chat_server.py
+++ chat_server.py
@@ -10,3 +10,21 @@
 #     -
 #
 # ----------------------------------------------------------------------------
+
+# Create socket
+
+# Bind to localhost and port xxxx
+
+# Listen for a connection
+
+# While connected
+
+    # Server calls recv to receive data
+
+    # Server prints data, then prompts for a reply
+
+    # If reply is "/q", server quits
+
+    # Else, server sends the reply
+
+# Close sockets
